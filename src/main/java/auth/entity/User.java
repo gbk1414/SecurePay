0947786c6f8f744f02b872919f27ebd849f9e6db
gbk1414 <gbk1414@gmail.com>
2024-05-22 12:09:06 +0900

    feat(User.java, AccessLog.java, AccessToken.java,...): user와 관련된 Entity 생성

---
+++ src/main/java/auth/entity/User.java
@@ -0,0 +1,62 @@
+package auth.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String name;
+
+	@Column
+	private String email;
+
+	@Column
+	private String password;
+
+	@Column(length = 20)
+	private String phone;
+
+	@Column
+	@Enumerated(value = EnumType.STRING)
+	private UserRole role;
+
+	@Column
+	private String regNo;
+
+	@Column(columnDefinition = "boolean default false")
+	private boolean isPersonalInfoVerified;
+
+	@OneToMany(mappedBy = "user")
+	private List<AccessToken> accessTokens;
+
+	@OneToMany(mappedBy = "user")
+	private List<RefreshToken> refreshTokens;
+
+	@OneToMany(mappedBy = "user")
+	private List<AccessLog> accessLogs;
+
+	public User() {}
+
+	public User(String name, String email, String password, String phone, UserRole role) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.role = role;
+	}
+}
